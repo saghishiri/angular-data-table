40245b024b801ec3712da05e9b57a0c1b8bd571c
saghishiri <saghishiri@gmail.com>
2020-07-20 19:20:52 +0430

    show demo.json data in table

---
+++ src/app/view/data-table/data-table.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {MainService} from "../../core/services/main.service";
+
+@Component({
+  selector: 'app-data-table',
+  templateUrl: './data-table.component.html',
+  styleUrls: ['./data-table.component.css']
+})
+export class DataTableComponent implements OnInit {
+  displayedColumns: string[] = ['row', 'name', 'Date', 'Vol', 'Low', 'Close', 'High', 'sma_20', 'Vol_dev_20', 'Vol_sma_20', 't_Vol', 'strong_buy', 'buy', 'weak_buy',];
+  dataSource: any[] = [];
+  loading: boolean = false;
+
+  constructor(private mainService: MainService) {
+  }
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData() {
+    this.mainService.getData().subscribe(res => {
+      console.log(res);
+      this.dataSource = res;
+      this.loading = false;
+    }, error => {
+      this.loading = false;
+    })
+  }
+
+}
