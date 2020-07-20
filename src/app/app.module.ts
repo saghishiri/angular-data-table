ceb53030cb2014825962920a826e54601a7e6813
saghishiri <saghishiri@gmail.com>
2020-07-20 15:23:29 +0430

    generate main service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DataTableComponent} from './data-table/data-table.component';
+import {MatTableModule} from "@angular/material/table";
+import {MainService} from "./services/main.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataTableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule
   ],
-  providers: [],
+  providers: [MainService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
